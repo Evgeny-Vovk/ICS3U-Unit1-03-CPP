151c922f2957b7ff108e872ce98c84a8e5bbd693
Evgeny Vovk <Evgeny.vovk@mths.ca>
2022-09-15 15:19:34 +0000

    Finished Unit1-03 work

---
+++ ICS3U-Unit1-03.cpp
@@ -0,0 +1,13 @@
+// Copyright(c) 2022 Evgeny Vovk All rights reserved
+//
+// Created by : Evgeny Vovk
+// Created on : September 2022
+// ICS3U-Unit1-03.cpp File, learning how style properly in C++
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!\n";
+
+    std::cout << "\nDone.\n";
+}
